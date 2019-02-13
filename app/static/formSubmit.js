b53c12d4e3e9d0f7c402b1cb1fc0bcc76951344c
yinyinflorence <869115528@qq.com>
2019-02-12 20:51:48 -0500

    add comments and form

---
+++ app/static/formSubmit.js
@@ -0,0 +1,6 @@
+console.log("check import");
+$(function () {
+	$(".btn").on('click', function() {
+		$("form").append($("#inputText").val());
+	console.log("hello");});
+});
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import json
 
 import flask
+from flask import request
 
 from app import app
 from app.forms import *
@@ -10,7 +11,15 @@ def index():
     content = '<p>To add content, modify <code>views.py</code>.<p>'
     return flask.render_template('main.html', content=content)
 
-@app.route('/comments')
-def datafunction():
-    return "hello"
-    
+@app.route('/comments/')
+def commentPage():
+    content = '<p>Hello I\'m comment page.<p>'
+    return flask.render_template('comments.html', content=content)
+
+@app.route('/commentsResult/')
+def commentResult():
+	if request.method == 'GET':
+
+	    content = request.args.get('inputText')
+	    return flask.render_template('comments.html', content=content)
+
